7c4dac6e8ee25b5d1cf074848981617ea3ab9434
pi <upabst55@gmail.com>
2020-05-16 18:07:13 +0200

    Es wird nur noch ein Auto erzeugt und angezeigt.

--- Beispiele/Kapitel 12/Beispiel_12_1.py
+++ Beispiele/Kapitel 12/Beispiel_12_1.py
@@ -8,21 +8,15 @@ import Auto
 def main():
     # Hauptprogramm
     auto_eins = Auto.Auto("Kia", "Silber", 50, 3)
-    auto_zwei = Auto.Auto("Bentley", "Weiß", 150, 5)
 
     print("\nDaten von Auto eins:")
     auto_eins.zeige_daten()
 
-    print("\nDaten von Auto zwei:")
-    auto_zwei.zeige_daten()
-
     print("\nDie Autos fahren ein wenig durch die Gegend...")
 
     auto_eins.strecke_fahren(64)
-    auto_zwei.strecke_fahren(128)
 
     print("Kilometerstand des ersten Autos:", auto_eins.kilometerstand)
-    print("Kilometerstand des zweiten Autos:", auto_zwei.kilometerstand)
 
 
 main()
